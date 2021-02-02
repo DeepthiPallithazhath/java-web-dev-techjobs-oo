7b10cef43578093ba34c7416e67cedd1a184938b
DeepthiPallithazhath <deepthips87@gmail.com>
2021-02-02 17:05:55 -0600

    1 added 5 modified

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -2,46 +2,47 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class CoreCompetency {
-    private int id;
+public class CoreCompetency extends JobField {
+    /*private int id;
     private static int nextId = 1;
     private String value;
 
     public CoreCompetency() {
         this.id = nextId;
         nextId++;
-    }
+    }*/
 
     public CoreCompetency(String value) {
-        this();
-        this.value = value;
+        /*this();
+        this.value = value;*/
+        super(value);
     }
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
+   /* @Override
     public String toString() {
         if(value=="")
             this.value="Data not avaliable";
         return value;
-    }
+    }*/
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof CoreCompetency)) return false;
-        CoreCompetency that = (CoreCompetency) o;
-        return id == that.id;
+        CoreCompetency coreCompetency = (CoreCompetency) o;
+        return getId() == coreCompetency.getId();
     }
 
-    @Override
+    /*@Override
     public int hashCode() {
         return Objects.hash(id);
-    }
+    }*/
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
 
-    public int getId() {
+  /*  public int getId() {
         return id;
     }
 
@@ -51,6 +52,6 @@ public class CoreCompetency {
 
     public void setValue(String value) {
         this.value = value;
-    }
+    }*/
 //  ONLY a getter for the 'id' field.
 }
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -2,31 +2,13 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
+public class PositionType extends JobField{
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
-    @Override
-    public String toString() {
-        if(value=="")
-            this.value="Data not avaliable";
-        return  value;
-
-    }
-
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
 
@@ -34,27 +16,9 @@ public class PositionType {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+        PositionType positionType = (PositionType) o;
+        return getId() == positionType.getId();
     }
 
 
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
